3ab23bd7dc1dd0e0dd0f7b25773419bc5c236d37
BrenaM <brenamonteiro_@hotmail.com>
2021-03-31 14:47:06 -0300

    add funcionalidade as rotas get e post

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
-const { request, response } = require('express');
 const express = require('express');
 const app = express();
-// console.log(app);
+const { v4:uuidv4 } = require('uuid');
+
+app.use(express.json());
+
+const projects = [];
 
 /**
- * Get: buscar informações do back-end
- * Post: criar informções no back-end
- * Put/PATCH: Alterar uma informação no back-end
- * Delete: Deletar informações do back-end
+ * -- Parâmetros --
+ * Query Params: funciona dentro do GET. Vamos usar principalmente para filtros e 
+ *                  paginação;
+ * Route Params: Identificar recursos na hr de atualizar ou deletar(). 
+ * Request Body: Traz todo o conteudo da aplicação. o resto do conteudo na hr de 
+ *                  criar ou editar um recurso
+ * 
  */
 
-//criando uma rota o 1° parametro é o endereço
-app.get('/projects', (request, response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 100'
-    ]);
-})
+ app.get('/projects', (request, response) => {
+    /*const query = request.query; 
+    trocamos o comando assima por uma versão desestruturada.
+    Trazendo tudo que tem dentro do query de forma separada {title, owner}
+    *///const {title, owner} = request.query;
+
+    return response.json(projects);
+});
 
 app.post('/projects', (request, response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-        'Projeto 4',
-        'Projeto 5'
-    ]); 
+    const {title, owner} = request.body;
+
+    const project = { id: uuidv4(), title, owner};
+
+    projects.push(project); //esse push vai jogar a criação do nosso projeto para o nosso array
+
+    return response.json(project); //sempre retorna o projeto recem criado e nunca exibir a lista inteira 
 });
 
 app.put('/projects/:id', (request, response) => {
+    const params = request.params;
+
+    console.log(params);
+
     return response.json([
         'Projeto 56',
         'Projeto 2',
@@ -42,8 +52,7 @@ app.put('/projects/:id', (request, response) => {
 app.delete('/projects/:id', (request, response) => {
     return response.json([
         'Projeto 56',
-        'Projeto 2'
-        
+        'Projeto 2'   
     ]);
 });
 

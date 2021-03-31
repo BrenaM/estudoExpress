28e00cc4fc68de09ab734b3b3505ffef9eaa579c
BrenaM <brenamonteiro_@hotmail.com>
2021-03-31 11:15:32 -0300

    usando put, delete e post

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
+const { request, response } = require('express');
 const express = require('express');
 const app = express();
-
 // console.log(app);
+
+/**
+ * Get: buscar informações do back-end
+ * Post: criar informções no back-end
+ * Put/PATCH: Alterar uma informação no back-end
+ * Delete: Deletar informações do back-end
+ */
+
 //criando uma rota o 1° parametro é o endereço
-app.get('/', (request, response) => {
-    response.send('Olá, Mundo!');
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 100'
+    ]);
 })
 
-app.listen(3000, () => {
-    console.log('Servidor rodando!');
-})
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4',
+        'Projeto 5'
+    ]); 
+});
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 56',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4',
+        'Projeto 5'
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 56',
+        'Projeto 2'
+        
+    ]);
+});
+
+app.listen(3333);
+
+//app.post()
+// app.listen(3000, () => {
+//     console.log('Servidor rodando!');
+// })

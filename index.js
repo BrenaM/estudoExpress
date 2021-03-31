43955cbd74a73a8ce41cb48614bc8a45b0fcd269
BrenaM <brenamonteiro_@hotmail.com>
2021-03-31 10:17:00 -0300

    definindo uma rota usando express

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
-
 const app = express();
 
-console.log(app);
+// console.log(app);
+//criando uma rota o 1° parametro é o endereço
+app.get('/', (request, response) => {
+    response.send('Olá, Mundo!');
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando!');
+})
